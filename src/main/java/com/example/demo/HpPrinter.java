e750e5215f06452bb29ca2c7ee9d5cfe9c6cf46b
tsungilin <tsungilin@bitdigitaltech.com>
2021-12-26 09:50:40 +0800

    使用postConstruct初始化Bean

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -2,11 +2,22 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class HpPrinter implements Printer {
 
+    private int count;
+
+    @PostConstruct
+    public void init() {
+        count = 10;
+    }
+
     @Override
     public void print(String message) {
+        count--;
         System.out.println("Hp印表機" + message);
+        System.out.println("剩餘使用次數： " + count);
     }
 }
--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @Autowired
-    @Qualifier("myPrinter")
     private Printer printer;
-
-    @Autowired
-    private String str;
 
     @GetMapping("/test")
     public String test() {
-        System.out.println(str);
         printer.print("Hello world");
         return "Hello world";
     }
