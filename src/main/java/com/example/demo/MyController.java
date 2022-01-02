f458dc537e817e57e4a25a39fcc3244171d37841
tsungilin <tsungilin@bitdigitaltech.com>
2022-01-02 11:35:50 +0800

    回傳json格式

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,26 +1,31 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/detail")  //此路徑會對應到每個方法, EX: .../detail/test
 @RestController
 public class MyController {
 
-    @Autowired
-    private Printer printer;
-
-    @GetMapping("/test")
-    public String test() {
-        printer.print("Hello world");
-        return "Hello world";
+    @GetMapping("/product")
+    public Store test() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductList(list);
+        return store;
     }
 
     @RequestMapping("/user")
-    public String user() {
-        return "名字是Ken";
+    public Student user() {
+        Student student = new Student();
+        student.setName("Ken");
+        return student;
     }
 
 }
