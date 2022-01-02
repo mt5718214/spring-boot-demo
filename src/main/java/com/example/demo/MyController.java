3bd797120f6752eaba072829778fea7ba02fc7e6
tsungilin <tsungilin@bitdigitaltech.com>
2022-01-02 09:24:57 +0800

    url路徑對應: requestMapping

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/detail")  //此路徑會對應到每個方法, EX: .../detail/test
 @RestController
 public class MyController {
 
@@ -14,6 +16,11 @@ public class MyController {
     public String test() {
         printer.print("Hello world");
         return "Hello world";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "名字是Ken";
     }
 
 }
